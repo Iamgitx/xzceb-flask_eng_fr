2c38c16e3c37b15ace3cb3bdd8dd3a5f73a7cfc6
Iamgitx <vhodvmail@gmail.com>
2023-06-19 06:58:15 -0400

    Temporary changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,16 +5,16 @@ from translator import english_to_french, french_to_english
 
 class TestenglishToFrench(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(english_to_french('hello'), 'bonjour') # Hello is given as input the output is Bonjour.
-        self.assertEqual(english_to_french('yes'), 'oui')  # Yes is given as input the output is Oui.
-        self.assertNotEqual(english_to_french('house'), 'house')  # House is given as input the output is not House.
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertEqual(english_to_french('yes'), 'oui')
+        self.assertNotEqual(english_to_french('hello'), 'hello')
         
 
 class TestfrenchToEnglish(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(french_to_english('bonjour'), 'hello') # Bonjour is given as input the output is Hello.
-        self.assertEqual(french_to_english('oui'), 'yes') # Oui is given as input the output is Yes.
-        self.assertNotEqual(french_to_english('maison'), 'maison') # Maison is given as input the output is not Maison.
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertEqual(french_to_english('oui'), 'yes')
+        self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
         
 if __name__ == "__main__":    
     unittest.main()
