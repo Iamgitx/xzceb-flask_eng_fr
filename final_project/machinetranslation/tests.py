2a46e31a6308f3cf09a1141185b5abf4855881ae
Iamgitx <vhodvmail@gmail.com>
2023-06-20 01:45:08 -0400

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,14 +2,14 @@ import unittest
 from translator import english_to_french, french_to_english
 
 
-class Test_etf(unittest.TestCase): 
+class TestEtf(unittest.TestCase): 
     def test1(self): 
         self.assertEqual(english_to_french('hello'), 'bonjour')
         self.assertEqual(english_to_french('yes'), 'oui')
         self.assertNotEqual(english_to_french('hello'), 'hello')
         
 
-class Test_fte(unittest.TestCase): 
+class TestFte(unittest.TestCase): 
     def test1(self): 
         self.assertEqual(french_to_english('bonjour'), 'hello')
         self.assertEqual(french_to_english('oui'), 'yes')
